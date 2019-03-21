bd3fbff66b6915b14b74dcde9cc83f78d71a526e
Marc Cometa <menfcometa@gmail.com>
2019-03-21 22:35:26 +0800

    Production ready

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import "./scss/index.scss"
+import "./scss/index.scss";
 
 export default class App {
-	constructor() {
-		this.initalized = false;
-	}
+  constructor() {
+    this.initalized = false;
+  }
 
-	init() {
-		if (this.initialized) {
-			this.destroy();
-		}
+  init() {
+    if (this.initialized) {
+      this.destroy();
+    }
 
-		this.go();
+    this.go();
 
-		this.initialized = true;
-	}
+    this.initialized = true;
+  }
 
 
-	destroy() {
-		// assign to `null` elements that are creted by this initially
+  destroy() {
+    // assign to `null` elements that are creted by this initially
 
-		// unset initialized value to true
-		this.initialized = false;
-	}
+    // unset initialized value to true
+    this.initialized = false;
+  }
 
-	go() {
+  go() {
 
-		return document.addEventListener("DOMContentLoaded", function () {
-			console.log("Stack63 website, now running.")
-		})
+    return document.addEventListener("DOMContentLoaded", function () {
+      console.log("Stack63 website, now running.")
+    })
 
-	}
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,76 @@ const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const SvgUrlLoader = require('svg-url-loader');
+
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+if (devMode) {
+  console.log(">>> Deploying to production...")
+}
+
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		library: 'Stack63',
-		libraryTarget: 'umd',
-		libraryExport: 'default',
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js'
-	},
-	module: {
-		rules: [{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: ['babel-loader']
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'postcss-loader',
-					'sass-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		// new uglifyJsPlugin(),
-		new HTMLWebpackPlugin({
-			template: path.resolve(__dirname, 'index.html')
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-	]
+  entry: './src/index.js',
+  output: {
+    library: 'Stack63',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[hash].js'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: {
+          loader: 'svg-url-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    // new uglifyJsPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new CopyPlugin([{
+      from: 'src/**/*.png',
+      to: 'assets/images/[name].[ext]'
+    }]),
+    new CopyPlugin([{
+      from: 'src/**/*.svg',
+      to: 'assets/images/[name].[ext]'
+    }])
+  ]
 };
